395bd75c69dee0558cee3990c5c3fdfa8446df06
arielnux <arielnux@users.noreply.github.com>
2014-12-04 00:49:59 +0200

    Create negotiate.cpp
    priliminary negotiate.cpp code

---
+++ negotiate.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : negotiate.cpp
+// Author      : zennet R\D
+// Version     : 0
+// Copyright   : zennet contract_functions cpp file
+// Description : zennet contract functions code
+//============================================================================
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include "contract.h"
+using namespace std;
+
+bool negotiate(contract_t provider_contract,contract_t publisher_contract)
+{
+	bool both_parties_sign=false;
+	while(!both_parties_sign)
+	{
+// TO DO - some functions negotiating again
+	}
+	return both_parties_sign;
+}
+
+
