258223cfc8d83164f7da0c01930d61c3149d9b36
halmut <rhalmut@gmail.com>
2014-12-18 01:27:54 +0200

    Create provider_micro_payment.h

---
+++ provider_micro_payment.h
@@ -0,0 +1,141 @@
+//============================================================================
+// Name        : provider_micro_payment.h
+// Author      : zennet R\D
+// Version     : 0
+// Copyright   : zennet provider_micro_payment header file
+// Description : zennet provider_micro_payment code
+//============================================================================
+
+#ifndef PROVIDER_MICRO_PAYMENT_H_
+#define PROVIDER_MICRO_PAYMENT_H_
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include "providerwallet.h"
+#include "contractRavit.h"
+using namespace std;
+
+typedef std::string provider_pubkey_t;
+typedef vector<pubkey_t>  inputs_vector;
+
+/* Class for the public key*/
+class CPubKey {
+public:
+    // Construct an invalid public key.
+    CPubKey() {
+        Invalidate();
+    }
+
+	// Function Set is for initialize a public key using begin/end iterators to byte data.
+    template<typename T>
+    void Set(const T pbegin, const T pend) {
+        int len = pend == pbegin ? 0 : GetLen(pbegin[0]);
+        if (len && len == (pend-pbegin))
+            memcpy(vch, (unsigned char*)&pbegin[0], len);
+        else
+            Invalidate();
+    }
+
+    // Construct a public key using begin/end iterators to byte data.
+    template<typename T>
+    CPubKey(const T pbegin, const T pend) {
+        Set(pbegin, pend);
+    }
+
+    // Construct a public key from a byte vector.
+    CPubKey(const std::vector<unsigned char> &vch) {
+        Set(vch.begin(), vch.end());
+    }
+
+private:
+    // Just store the serialized data.
+    // Its length can very cheaply be computed from the first byte.
+    unsigned char vch[65];
+
+    // This function will set this key data to be invalid
+    void Invalidate() {
+        vch[0] = 0xFF;
+    }
+
+    // This function will compute the length of a pubkey with a given first byte.
+    unsigned int static GetLen(unsigned char chHeader) {
+        if (chHeader == 2 || chHeader == 3)
+            return 33;
+        if (chHeader == 4 || chHeader == 6 || chHeader == 7)
+            return 65;
+        return 0;
+    }
+
+};
+
+
+/******************************************************************/
+
+/* Class for the transactions */
+class CTrx
+{
+public:
+	// Construct an invalid private key.
+    CTrx() : TrxValid(false) {
+        LockObject(vch);
+    }
+
+	// Destructor 
+    ~CTrx() {
+        UnlockObject(vch);
+    }
+
+	// This function will set the lock time
+	int set_lock_time(double Lt);
+	// 	This function will request for the multi signature
+	int request_multi_sig();
+	//  This function will get the multi signature
+	sig_t get_multi_sig();
+	//	This function will set the input vector
+	int set_input_vector();
+	// 	This function will return the input vector
+	inputs_vector get_input_vector();
+
+
+private:
+	// Whether this private key is valid. We check for correctness when modifying the key data
+    bool TrxValid;
+
+	// The input vector
+	inputs_vector input;
+
+	// The multisig
+	sig_t multi_sig;
+
+	// The actual byte data
+    unsigned char vch[32];
+
+	double lock_time;
+};
+
+/*
+*/
+class CProvider
+{
+public:
+	// Constructors
+	CProvider();
+	CProvider(provider_pubkey_t providerpubkey);
+	// Distructor
+	~CProvider();
+	// create new public key to the provider (k2) 
+	provider_pubkey_t CreatePubkey();
+	// send the public key to the publisher (k2)
+	provider_pubkey_t SendPubkeyToPublisher();
+private:
+	provider_pubkey_t providerpubkey;
+	void SetDefault();
+	// Trx implement the provider's transactions (t1,t2)
+	struct Trx trx_t;
+	// ProviderWallet implement the provider's wallet operations
+	struct ProviderWallet provider_wallet_t;
+};
+
+#endif /* PROVIDER_H_ */
