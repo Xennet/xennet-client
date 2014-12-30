ed6f9a51f0c7cd9fb66102bdc898cab5b4f8a0a5
halmut <rhalmut@gmail.com>
2014-12-30 22:10:44 +0200

    Update provider_micro_payment.cpp
    TODOs

--- provider_micro_payment.cpp
+++ provider_micro_payment.cpp
@@ -58,19 +58,118 @@ inputs_vector CTrx::get_input_vector()
 CProvider::CProvider()
 {
 	SetDefault();
-};
+}
 
 CProvider::~CProvider()
 {
-};
+}
 
 bool CProvider::SetDefault()
 {
+	// TODO set all the CProvider members with default values
+
 	// reset all provider's variable to default value
 	//providerpubkey
-	return string::empty(providerpubkey);//.IsNullOrEmpty();
-};
+	return string::empty(provider_pubkey_t);//.IsNullOrEmpty();
+}
 
+// Recieve a specifications and rate vector on a constant given frequency 
+// according to the canonical benchmarks
+SpecAndRate_vector CProvider::Recieve_SpecAndRate_vector(input_frequency input_frequency_p, input_canonical_benchmarks_vector input_canonical_benchmarks_vector_p)
+{
+	// TODO get the spec and rate vector according to the two function's inputs
+	// TODO set the CProvider::SpecAndRate_vector_t and return it
+
+	// return the vector
+	return SpecAndRate_vector_t;
+}
+
+// Recieve Announcement from Publisher
+Publisher_Ann CProvider::Recieve_Publisher_Ann()
+{
+	// TODO recieve a Publisher announcement
+
+	// Set and return the CProvider::Publisher_Ann_t variable
+	return Publisher_Ann_t;
+}
+
+// Create Contract C2 with specifications and rate vector
+contract_c CProvider::Create_contract_c2(SpecAndRate_vector SpecAndRate_vector_p)
+{
+	// TODO Create a Contract c2 with the SpecAndRate_vector_p
+	// TODO return the contract
+	return contract_c2_t;
+}
+
+// Send Contract C2 to Publisher. function will return 0 is succeed else if not
+int CProvider::Send_Contract_C2_to_Publisher()
+{
+	// TODO Send the contract c2 to the Publisher
+
+	return 0;
+}
+
+// Upodate contract c2 if necessary. function will return 0 is succeed else if not
+int CProvider::update_contract_c2()
+{
+	// TODO update contract c2
+
+	return 0;
+}
+
+// create public key k2 to the provider  
+public_key CProvider::CreatePubkey()
+{
+	// TODO create public key k2 for the Provider and return it
+
+	return provider_pubkey_t;
+}
+
+// Signed contract c2 with public key k2. function will update the Contract_provider_sign_flag_t and will return 0 is succeed else if not
+int CProvider::signed_contract_c2()
+{
+	// TODO
+
+	return 0;
+}
+
+// Request public key k1 from Publisher
+public_key CProvider::request_pubkey_from_publisher()
+{
+	// TODO request public key k1 from Publisher
+
+	return publisher_pubkey_t;
+}
+
+// Recieve public key k1 from publisher. function will update the Contract_publisher_sign_flag_t and will return 0 is succeed else if not
+//int recieve_pubkey_from_publisher()
+//{
+//	// TODO
+//	return 0;
+//}
+
+// Update publisher signature k1 in contract c2. function will return 0 is succeed else if not
+int CProvider::update_publisher_sign_in_contract_c2()
+{
+	// TODO 
+
+	return 0;
+}
+
+// send the public key k2 to the publisher 
+public_key CProvider::SendPubkeyToPublisher()
+{
+	// TODO
+
+	return provider_pubkey_t;
+}
+
+
+
+
+
+
+/**********************************************
 provider_pubkey_t CProvider::CreatePubkey()
 {
 };
@@ -86,3 +185,41 @@ provider_pubkey_t CProvider::SendPubkeyToPublisher()
 };
 
 
+//
+pubkey_t pub_micro::get_pubkey (int which_pubkey)
+{
+// which_pubkey=0 for provider, which_pubkey=1 for publisher
+	if (which_pubkey==0)
+	{
+		return provider;
+	}
+	else
+	{
+		return publisher;
+	}
+}
+
+int pub_micro::set_pubkey (pubkey_t provider_pubkey,pubkey_t publisher_pubkey,int which_pubkey)
+{
+// which_pubkey=0 for provider, which_pubkey=1 for publisher
+// return 1 if pubkey is empty, return 0 if pubkey is ok
+	if (which_pubkey==0)
+	{
+		if(provider_pubkey.empty())
+		{
+			return 1;
+		}
+		provider=provider_pubkey;
+	}
+	else
+	{
+		if(provider_pubkey.empty())
+		{
+			return 1;
+		}
+		publisher=publisher_pubkey;
+	}
+	return 0;
+}
+*/
+ 
