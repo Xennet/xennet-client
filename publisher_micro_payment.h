baa789906bd72cce3e3e7622c7d1500a38780c59
arielnux <arielnux@users.noreply.github.com>
2014-12-31 15:59:27 +0200

    Update publisher_micro_payment.h

--- publisher_micro_payment.h
+++ publisher_micro_payment.h
@@ -27,47 +27,65 @@ typedef vector<pubkey_t>  inputs_vector;
 // 	tx_t class is for the "logical deposit box" class declaration
 class tx_t
 {
-friend class pub_micro;
+	friend class pub_micro;
 public:
+
+//	TO DO
+	void set_input_vector(pubkey_t pubkey);
+// 	TO DO
+	inputs_vector get_input_vector();
+//	TO DO
+	void set_output_object(pubkey_t pubkey,int which_tx);
+//	TO DO
+	output_object get_output_object();
 // 	TO DO
 	int request_multi_sig();
 //  TO DO
 	sig_t get_multi_sig();
 //	TO DO
-	int set_input_vector();
-// 	TO DO
-	inputs_vector get_input_vector();
+	int set_multi_sig(sig_t signature);
 //	TO DO
-	int set_output_object(output_object outputs);
+	int request_digital_signature();
 //	TO DO
-	output_object get_output_object();
+	int request_time_lock(int which_tx);
 //	TO DO
-	int set_lock_time(double lt);
+	int set_time_lock(double lt);
+//	To Do
+	int send_transaction();
 //	TO DO
 	tx_t();
 
 private:
+
 //	TO DO
-//	vector<pubkey_t> inputs_vector;
 	inputs_vector inputs;
 //  TO DO
-//	map<pubkey_t,coins_t> output_object;
 	output_object outputs;
 //	deposit_sig member is the deposit multi signature string declaration
 	sig_t multi_sig;
+//	publisher signature
+	sig_t signature;
 //  lock time of the transaction in days
-	double lock_time;
+	double time_lock;
 };
 
 
 class pub_micro
 {
+	//friend class tx_t;
 public:
 	int request_publisher_pubkey();
 	int request_provider_pubkey();
 	int set_pubkey (pubkey_t provider_pubkey,pubkey_t publisher_pubkey,int which_pubkey);
 	pubkey_t get_pubkey (int which_pubkey);
 	pub_micro();
+	int request_multi_sig();
+	//  the class member T1 is the Zencoins transaction
+	tx_t T1;
+	//  the class member T2 is the refund transaction
+	tx_t T2;
+	//	the updating transaction of Zencoins
+	tx_t T3;
 
 private:
 	//  the class member "publisher" is a string public key
@@ -75,11 +93,6 @@ private:
 
 	//  the class member "publisher" is a string public key
 		pubkey_t publisher;
-	//  the class member T1 is the Zencoins transaction
-		tx_t T1;
-	//  the class member T2 is the refund transaction
-		tx_t T2;
-
 };
 
 
