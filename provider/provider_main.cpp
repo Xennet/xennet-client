911670dc3a46418138202d223c47b15266719441
halmut <rhalmut@gmail.com>
2015-01-08 11:06:58 +0200

    Create provider_main.cpp

---
+++ provider/provider_main.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : provider_main.cpp
+// Author      : zennet
+// Version     :
+// Copyright   : zennet Provider main
+// Description : Provider main code
+//============================================================================
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include "provider_micro_payment.h"
+using namespace std;
+
+
+
+
+int main(int argc, char** argv)
+{
+	CProvider Provider_t;
+	cout<<"ZENNET Provider" << endl; 
+	return 0;
+}
