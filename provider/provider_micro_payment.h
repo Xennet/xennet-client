d88c85ef3a5b2d99980a623a3fc21b839f97afd2
halmut <rhalmut@gmail.com>
2015-01-08 11:02:46 +0200

    Update provider_micro_payment.h
    removing future code preparations

--- provider/provider_micro_payment.h
+++ provider/provider_micro_payment.h
@@ -14,10 +14,7 @@
 #include <vector>
 #include <string>
 #include "providerwallet.h"
-#include "contractRavit.h"
 using namespace std;
-
-//typedef vector<pubkey_t>  inputs_vector;
 
 // For class CProvider
 typedef std::string private_key;
@@ -28,103 +25,6 @@ typedef std::string input_canonical_benchmarks_vector;
 typedef std::string Publisher_Ann;
 typedef std::string contract_c;
 
-/* Class for the public key
-class CPubKey {
-public:
-    // Construct an invalid public key.
-    CPubKey() {
-        Invalidate();
-    }
-
-	// Function Set is for initialize a public key using begin/end iterators to byte data.
-    template<typename T>
-    void Set(const T pbegin, const T pend) {
-        int len = pend == pbegin ? 0 : GetLen(pbegin[0]);
-        if (len && len == (pend-pbegin))
-            memcpy(vch, (unsigned char*)&pbegin[0], len);
-        else
-            Invalidate();
-    }
-
-    // Construct a public key using begin/end iterators to byte data.
-    template<typename T>
-    CPubKey(const T pbegin, const T pend) {
-        Set(pbegin, pend);
-    }
-
-    // Construct a public key from a byte vector.
-    CPubKey(const std::vector<unsigned char> &vch) {
-        Set(vch.begin(), vch.end());
-    }
-
-private:
-    // Just store the serialized data.
-    // Its length can very cheaply be computed from the first byte.
-    unsigned char vch[65];
-
-    // This function will set this key data to be invalid
-    void Invalidate() {
-        vch[0] = 0xFF;
-    }
-
-    // This function will compute the length of a pubkey with a given first byte.
-    unsigned int static GetLen(unsigned char chHeader) {
-        if (chHeader == 2 || chHeader == 3)
-            return 33;
-        if (chHeader == 4 || chHeader == 6 || chHeader == 7)
-            return 65;
-        return 0;
-    }
-
-};
-
-*/
-/******************************************************************/
-
-/* Class for the transactions */
-class CTrx
-{
-public:
-	// Construct an invalid private key.
-    CTrx() : TrxValid(false) {
-        LockObject(vch);
-    }
-
-	// Destructor 
-    ~CTrx() {
-        UnlockObject(vch);
-    }
-
-	// This function will set the lock time
-	int set_lock_time(double Lt);
-	// 	This function will request for the multi signature
-	int request_multi_sig();
-	//  This function will get the multi signature
-	sig_t get_multi_sig();
-	//	This function will set the input vector
-	int set_input_vector();
-	// 	This function will return the input vector
-	inputs_vector get_input_vector();
-
-
-private:
-	// Whether this private key is valid. We check for correctness when modifying the key data
-    bool TrxValid;
-
-	// The input vector
-	inputs_vector input;
-
-	// The multisig
-	sig_t multi_sig;
-
-	// The actual byte data
-    unsigned char vch[32];
-
-	double lock_time;
-
-	// This function will check whether the 32-byte array pointed to by vch is valid keydata.
-    bool static Check(const unsigned char *vch);
-};
 
 /*
 	CProvider is the ZENNET Server which is a computer­ owner willing to participate and earn
@@ -166,9 +66,9 @@ private:
 	// function will reset all class members to default values
 	void SetDefault();
 	// Trx implement the provider's transactions (t1,t2)
-	class CTrx trx_t;
+//	class CTrx trx_t;
 	// ProviderWallet implement the provider's wallet operations
-	struct ProviderWallet provider_wallet_t;
+//	struct ProviderWallet provider_wallet_t;
 
 public:
 	// Constructors
@@ -199,11 +99,8 @@ public:
 	// Signed contract c2 with public key k2. function will update the Contract_provider_sign_flag_t and will return 0 is succeed else if not
 	int signed_contract_c2();
 
-	// Request public key k1 from Publisher
+	// Request and recieve public key k1 from Publisher
 	public_key request_pubkey_from_publisher();
-
-	// Recieve public key k1 from publisher. function will update the Contract_publisher_sign_flag_t and will return 0 is succeed else if not
-	//int recieve_pubkey_from_publisher();
 
 	// Update publisher signature k1 in contract c2. function will return 0 is succeed else if not
 	int update_publisher_sign_in_contract_c2();
