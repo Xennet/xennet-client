c61efff7669ea5e1b15927ba53b15e27389fa8de
arielnux <arielnux@users.noreply.github.com>
2015-01-12 08:23:23 +0200

    Update publisher_flow.cpp

--- publisher/publisher_flow.cpp
+++ publisher/publisher_flow.cpp
@@ -21,7 +21,9 @@ int publisher_flow()
 {
 
 	int error_type=0;
+	coins_t stop_condition=0;
 	bool agree=false;
+	coins_t coins_update=0;
 	pub_micro pub;
 	contract_t provider_contract;
 	contract_t publisher_contract;
@@ -58,24 +60,52 @@ int publisher_flow()
 	error_type=pub.T1.request_digital_signature();
 //	update T1 with publisher pubkey ?? or other inputs ??
 	pub.T1.set_input_vector(pub.get_pubkey(1));
-//  update T1 with multi signature as output and Zencoins
-	pub.T1.set_output_object(pub.T1.get_multi_sig(),1);
+//  	update T1 with multi signature as output and Zencoins
+	pub.T1.set_output_object(pub.T1.get_multi_sig(),pub.T1.request_coins(1));
 //	request time_lock for T2 from the publisher wallet
 	error_type=pub.T2.request_time_lock(2);
-//	update T2 as "all money refund" to the publisher
-//	an option to send the word "end" to finish the input from wallet process
+//	update T2 input with multi sugnature
 	pub.T2.set_input_vector(pub.T1.get_multi_sig());
-//	update T2 output to publisher address
-	pub.T2.set_output_object(pub.get_pubkey(1),2);
+//	update T2 output to publisher address as "all money refund"
+	pub.T2.set_output_object(pub.get_pubkey(1),pub.T1.get_output_object(pub.T1.get_multi_sig()));
 //	Send the refund transaction T2 to the provider
-	error_type=pub.T2.send_transaction();
+	error_type=pub.T2.send_transaction(0);
+//	Receive the signed refund transaction T2 from the provider
+	error_type=pub.T2.receive_transaction();
+//	verify the provider signature in the signed refund transaction T2
+	error_type=pub.T2.verify_signature(pub.T2.get_digital_signature());
+//	abort the contract if signature verification is failed
+	if(error_type==1)
+	{
+		return 1;
+	}
+//	send the signed transaction T1 to the provider
+	error_type=pub.T1.send_transaction(0);
+//	send the signed transaction T1 to the multi signature
+	error_type=pub.T1.send_transaction(1);
 
 
+	stop_condition=0;
+//	update T3 input with multi signature
+	pub.T3.set_input_vector(pub.T1.get_multi_sig());
+
+//						micropayment step
+	while(stop_condition<=pub.T1.get_output_object(pub.T1.get_multi_sig()))
+	{
+//	calculate the amounts of Zencoins in T3,which has been "balance change" in T1
+	coins_update=pub.T3.calculate_coins();
+//	update T3 output to publisher address
+	pub.T3.set_output_object(pub.get_pubkey(1),coins_update);
+
+//	sign with digital signature in T3
+	//pub.T3.set_digital_signature(pub.T1.get_digital_signature());
+	}
 
 
 
 	return error_type;
 }
+
 
 contract_t receive_contract()
 {
@@ -100,4 +130,3 @@ bool compare_contract(contract_t provider_contract,contract_t publisher_contract
 //	To Do
 	return agree;
 }
-
